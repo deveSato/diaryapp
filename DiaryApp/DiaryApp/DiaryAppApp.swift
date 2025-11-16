a6dda176af8775175a0649c5cd1b7a0b67521584
user <user@usernoMacBook-Air.local>
2025-11-16 11:43:21 +0900

    Initial xcode setup

---
+++ DiaryApp/DiaryApp/DiaryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryAppApp.swift
+//  DiaryApp
+//
+//  Created by user on 2025/11/16.
+//
+
+import SwiftUI
+
+@main
+struct DiaryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
